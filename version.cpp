849225232c561b70aa4799d596f6f289dcd24495
Asterlenist <asterlenist.channel@gmail.com>
2026-09-19 13:16:01 +1000

    Fix version string formatting in output
    я ещё раз проебался, я лучше бы в следующую пятницу как я ранее думал

--- version.cpp
+++ version.cpp
@@ -6,6 +6,6 @@ int main() {
     cout << R"(  / _ \ (_-<  _/ -_) '_| / -_) ' \| (_-<  _|)" << endl;
     cout << R"( /_/ \_\/__/\__\___|_| |_\___|_||_|_/__/\__|)" << endl;
     cout << "" << endl;
-    cout << "            Version "beta 1" | MIT License" << endl;
+    cout << "            Version beta 1 | MIT License" << endl;
     return 0;
 }

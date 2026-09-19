65f7e320fb7e8a8ca2bba542fcf03bad7ea08b90
Asterlenist <asterlenist.channel@gmail.com>
2026-09-19 02:56:22 +0000

    Add ASCII art header to genpass.cpp
    Added ASCII art header to the password generator.

--- genpass.cpp
+++ genpass.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <random>
 #include <string>
+using namespace std;
+
+cout << R"(   ______                          #####   #   ### ### )" << endl;
+cout << R"( .' ___  |                         #   #  # #  #   #   )" << endl;
+cout << R"(/ .'   \_|  .---.  _ .--.  ______  #   # #   # ### #   )" << endl;
+cout << R"(| |   ____ / /__\\[ `.-. ||______| ##### #####   # ### )" << endl;
+cout << R"(\ `.___]  || \__., | | | |         #     #   #   #   # )" << endl;
+cout << R"( `._____.'  '.__.'[___||__]        #     #   # ### ### )" << endl;
+cout << "" << endl;
 
 std::string generate_password(int length, bool use_upper, bool use_lower,
                               bool use_digits, bool use_special) {
